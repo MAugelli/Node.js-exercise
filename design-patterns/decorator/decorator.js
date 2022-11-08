876b9de839f11ec9b06567c7111d3ab128d86772
MAugelli <m.augelli2@gmail.com>
2022-11-08 19:42:36 +0100

    Inject-the-bike-dependencies

--- design-patterns/decorator/decorator.js
+++ design-patterns/decorator/decorator.js
@@ -1,21 +1,21 @@
 class Text {
-    constructor(text) {
-      this.string = text
-    }
-
-    toString() {
-      return this.string
-    }
+  constructor(text) {
+    this.string = text
   }
 
-  class Shout {
-    constructor(text) {
-      this.text = text
-    }
-    toString() {
-      return this.text.toString().toUpperCase()
-    }
+  toString() {
+    return this.string
   }
+}
 
-  console.log(new Text("Hello, I'm talking").toString())
-  console.log(new Shout(new Text("Hello, I'm shouting!")).toString())
+class Shout {
+  constructor(text) {
+    this.text = text
+  }
+  toString() {
+    return this.text.toString().toUpperCase()
+  }
+}
+
+console.log(new Text("Hello, I'm talking").toString())
+console.log(new Shout(new Text("Hello, I'm shouting!")).toString())
